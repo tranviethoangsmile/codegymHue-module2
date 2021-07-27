2c4ba723529e1c3c671a5530b7c8db340ef3643c
tranviethoangsmile <tranviethoangsmile@gmail.com>
2021-07-27 16:32:57 +0700

    update

--- src/com/codegymhueJava/Main.java
+++ src/com/codegymhueJava/Main.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	int rate = 23000;
-	int usd;
-	Scanner input = new Scanner(System.in);
-        System.out.println("input usd:");
-        usd = input.nextInt();
-        int vnd = usd * rate;
-        System.out.println("VNĐ: " + vnd);
+        double money,month,interest;
+        Scanner input = new Scanner(System.in);
+        System.out.println("input money:");
+        money = input.nextDouble();
+        System.out.println("input month: ");
+        month = input.nextDouble();
+        System.out.println("input interest");
+        interest = input.nextDouble();
+        double interestRate = money * (interest/100)/12 *month;
+        System.out.println("money: " + money
+                + " " + "interest: " + interest + " "
+                + "month " + month + " = " + interestRate);
+
     }
 }
